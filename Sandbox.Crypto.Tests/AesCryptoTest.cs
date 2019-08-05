8bff45dba27ccbad8e5df3d7af7c9bc238d3a9e6
Kashif Soofi <kashif.soofi@tandem.co.uk>
2019-08-05 18:24:53 +0100

    Solution file and AesCrypto

---
+++ Sandbox.Crypto.Tests/AesCryptoTest.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Sandbox.Crypto.Tests
+{
+    public class AesCryptoTest
+    {
+        private readonly string _key = "56625DFA-A5DD-49F8-B640-75070C94D8BC";
+        private readonly string _plainText = "Here is some data to encrypt!";
+        private readonly string _cipherText = "";
+
+        [Fact]
+        public void Encrypt_should_encrypt_plainText()
+        {
+            var aesCrypto = new AesCrypto();
+
+            var encrypted = aesCrypto.Encrypt(_plainText, _key);
+
+            encrypted.Should().NotBeNullOrWhiteSpace();
+            encrypted.Should().NotBe(_plainText);
+        }
+
+        [Fact]
+        public void Decrypt_should_decrypt_cipherText()
+        {
+            var aesCrypto = new AesCrypto();
+
+            var decrypted = aesCrypto.Decrypt(_cipherText, _key);
+
+            decrypted.Should().NotBeNullOrWhiteSpace();
+            decrypted.Should().Be(_plainText);
+        }
+    }
+}
---
+++ Sandbox.Crypto/AesCrypto.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Sandbox.Crypto
+{
+    public class AesCrypto
+    {
+
+        public string Encrypt(string plainText, string key)
+        {
+            var keyBytes = Encoding.UTF8.GetBytes(key);
+            using (var aes = Aes.Create())
+            {
+                aes.Key = keyBytes;
+
+                var cryptoTransform = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                var plainTextAsBytes = Encoding.UTF8.GetBytes(plainText);
+                var cipherText = Encrypt(plainTextAsBytes, cryptoTransform);
+
+                var data = new byte[cipherText.Length + aes.IV.Length + 1];
+                data[0] = (byte) aes.IV.Length;
+                Array.Copy(aes.IV, 0, data, 1, aes.IV.Length);
+                Array.Copy(cipherText, 0, data, aes.IV.Length + 1, cipherText.Length);
+                return Convert.ToBase64String(data);
+            }
+        }
+
+        public string Decrypt(string cipherText, string key)
+        {
+            var data = Convert.FromBase64String(cipherText);
+            byte ivSize = data[0];
+            var iv = new byte[ivSize];
+            Array.Copy(data, 1, iv, 0, ivSize);
+            var encrypted = new byte[data.Length - ivSize - 1];
+            Array.Copy(data, ivSize + 1, encrypted, 0, encrypted.Length);
+
+            var keyBytes = Encoding.UTF8.GetBytes(key);
+            using (var aes = Aes.Create())
+            {
+                aes.Key = keyBytes;
+                aes.IV = iv;
+
+                var cryptoTransform = aes.CreateEncryptor(aes.Key, aes.IV);
+                return Decrypt(encrypted, cryptoTransform);
+            }
+        }
+
+        private byte[] Encrypt(byte[] data, ICryptoTransform cryptoTransform)
+        {
+            if (data == null || data.Length <= 0)
+                throw new ArgumentException(nameof(data));
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (var cryptoStream = new CryptoStream(memoryStream, cryptoTransform, CryptoStreamMode.Write))
+                {
+                    using (var writer = new StreamWriter(cryptoStream))
+                    {
+                        writer.Write(data);
+                    }
+                }
+
+                return memoryStream.ToArray();
+            }
+        }
+
+        private string Decrypt(byte[] data, ICryptoTransform cryptoTransform)
+        {
+            if (data == null || data.Length <= 0)
+                throw new ArgumentException(nameof(data));
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (var cryptoStream = new CryptoStream(memoryStream, cryptoTransform, CryptoStreamMode.Read))
+                {
+                    using (var reader = new StreamReader(cryptoStream))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+}
